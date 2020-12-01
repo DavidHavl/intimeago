4b651c25d29faf85b438f5d8286c3590dec72069
davidhavl <contact@davidhavl.com>
2020-12-01 20:13:44 +0100

    Refactor and add prepend-text option

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,7 +8,8 @@ import { LocaleFunction, LocaleName, SetupOptions, TDatetime, TimerPool } from '
 import { diffSec, formatDiff, nextInterval } from './utils/date'
 import { getLocale, importLocale, isLocaleImported } from './utils/locale'
 
-export const DATETIME_ATTRIBUTE_NAME = 'data-datetime'
+export const DATETIME_ATTRIBUTE_NAME = 'data-intimeago-datetime'
+export const PREPEND_TEXT_ATTRIBUTE_NAME = 'data-intimeago-prepend-text'
 export const UPDATE_EVENT_NAME = 'intimeago-update'
 
 const TIMER_POOL: TimerPool = {}
@@ -19,7 +20,7 @@ const TIMER_ID_ATTRIBUTE_NAME = 'intimeago-timer-id'
  * @param node
  */
 const clearTimer = (node: HTMLElement): void => {
-  if (node.hasAttribute(TIMER_ID_ATTRIBUTE_NAME)) {
+  if (node && node.hasAttribute(TIMER_ID_ATTRIBUTE_NAME)) {
     const timerId = parseInt(String(node.getAttribute(TIMER_ID_ATTRIBUTE_NAME)))
     clearTimeout(timerId)
     delete TIMER_POOL[timerId]
@@ -27,51 +28,65 @@ const clearTimer = (node: HTMLElement): void => {
 }
 
 function runSingle(node: HTMLElement, datetime: string, localeFunction: LocaleFunction, options: SetupOptions): void {
-  // clear the node's exist timer
+  // clear the node's existing timer
   clearTimer(node)
-
-  const { relativeDate } = options
+  // check if still in the dome (has not been detached)
+  if (!node.isConnected) {
+    return
+  }
+    const { relativeDate } = options
 
   // get diff seconds
   const diff = diffSec(datetime, relativeDate)
+  const prepend = node.getAttribute(PREPEND_TEXT_ATTRIBUTE_NAME)
   // render
-  node.innerText = formatDiff(diff, localeFunction)
+  node.innerText = (prepend ? prepend : '') + formatDiff(diff, localeFunction)
 
   // Dispatch the event.
   // @ts-ignore
   node.dispatchEvent(new CustomEvent(UPDATE_EVENT_NAME, { detail: { diff } }))
 
+  const nextInt = nextInterval(diff)
+
   const timerId = (setTimeout(() => {
     runSingle(node, datetime, localeFunction, options)
-  }, Math.min(Math.max(nextInterval(diff), 1) * 1000, 0x7fffffff)) as unknown) as number
+  }, Math.min(Math.max(nextInt, 1) * 1000, 0x7fffffff)) as unknown) as number
 
-  // there is no need to save node in object. Just save the key
-  TIMER_POOL[timerId] = 0
+  // Just the key is fine
+  TIMER_POOL[timerId] = 1
   node.setAttribute(TIMER_ID_ATTRIBUTE_NAME, String(timerId))
 }
 
 /**
- * Remove from dom element
- * @param node - the node to remove the functionality from
+ * Remove from one or more elements
+ * @param nodes - the node/s to remove the functionality from
  */
-export function remove(node?: HTMLElement): void {
-  // clear one node
-  if (node) {
-    clearTimer(node)
-  } else {
-    // clear all timers
+export function remove(nodes: HTMLElement | HTMLElement[] | Node | NodeList): void {
+  // clear one or more known nodes
+  if (nodes) {
     // @ts-ignore
-    Object.keys(TIMER_POOL).forEach(clear)
+    const nodeList: HTMLElement[] = nodes.length ? nodes : [nodes]
+    for (const key in Object.keys(nodeList)) {
+      clearTimer(nodeList[key])
+    }
+  }
+  // else clear all timers
+  else {
+    // @ts-ignore
+    Object.keys(TIMER_POOL).forEach((timerId: number) => {
+      clearTimeout(timerId)
+      delete TIMER_POOL[timerId]
+    })
   }
 }
 
 /**
  * Setup dom element/s
- * @param elements
- * @param locale
+ * @param nodes {HTMLElement | HTMLElement[] | NodeList}
+ * @param locale {LocaleName}
  * @param options
  */
-export function setup(elements: HTMLElement | HTMLElement[] | NodeList, locale?: LocaleName, options?: SetupOptions) {
+export function setup (nodes: HTMLElement | HTMLElement[] | NodeList, locale?: LocaleName, options?: SetupOptions) {
   locale = locale || 'en_US'
   // import needed locale
   if (!isLocaleImported(locale)) {
@@ -79,14 +94,14 @@ export function setup(elements: HTMLElement | HTMLElement[] | NodeList, locale?:
   }
 
   // @ts-ignore
-  const elementList: HTMLElement[] = elements.length ? elements : [elements]
-  for (const key in Object.keys(elementList)) {
-    if (elementList[key].hasAttribute(DATETIME_ATTRIBUTE_NAME)) {
-      runSingle(elementList[key], String(elementList[key].getAttribute(DATETIME_ATTRIBUTE_NAME)), getLocale(locale), options || {})
+  const nodeList: HTMLElement[] = nodes.length ? nodes : [nodes]
+  for (const key in Object.keys(nodeList)) {
+    if (nodeList[key].hasAttribute(DATETIME_ATTRIBUTE_NAME)) {
+      runSingle(nodeList[key], String(nodeList[key].getAttribute(DATETIME_ATTRIBUTE_NAME)), getLocale(locale), options || {})
     }
   }
 
-  return elementList
+  return nodeList
 }
 
 /**
@@ -95,7 +110,7 @@ export function setup(elements: HTMLElement | HTMLElement[] | NodeList, locale?:
  * @param locale
  * @param options
  */
-export const format = (date: TDatetime, locale?: LocaleName, options?: SetupOptions): string => {
+export function format (date: TDatetime, locale?: LocaleName, options?: SetupOptions): string {
 
   locale = locale || 'en_US'
   // import needed locale
