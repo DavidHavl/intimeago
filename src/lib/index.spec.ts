2bde3d6ce255061dde2194a9c94dc4ddde324c9d
davidhavl <contact@davidhavl.com>
2020-12-01 20:56:52 +0100

    Update dist files

--- src/lib/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import test from 'ava'
-
-import { format } from './index'
-
-test('format', (t) => {
-  const dateObj = new Date()
-  dateObj.setTime(new Date().getTime() + 30900)
-  t.is(format(dateObj), 'in 30 seconds')
-})
